77092706122b9a71b580e1d818985903e3aefb9e
mellifv <124873001+mellifv@users.noreply.github.com>
2025-12-02 16:28:37 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
+import { LanguageProvider } from './context/LanguageContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
 );
+
