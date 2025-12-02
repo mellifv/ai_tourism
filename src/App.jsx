91a28c0857faf5735a0f6307f6ff5b796d80e009
mellifv <124873001+mellifv@users.noreply.github.com>
2025-12-02 16:26:58 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-// src/App.jsx
+// App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { LanguageProvider } from "./context/LanguageContext"; // Add this
 import Home from "./pages/Home";
 import VideoGuides from "./pages/VideoGuides";
 import Nav from "./components/Nav";
 import Itineraries from "./pages/Itineraries";
 import CityInsights from "./pages/CityInsights";
+// Remove this line: import GoogleTranslateWidget from "./pages/translate";
 
 export default function App() {
   return (
-    <LanguageProvider> {/* Wrap everything with LanguageProvider */}
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/videos" element={<VideoGuides />} />
-          <Route path="/itineraries" element={<Itineraries />} />
-          <Route path="/insights" element={<CityInsights />} />
-        </Routes>
-      </Router>
-    </LanguageProvider>
+    <Router>
+      <Nav />
+      {/* Remove this: <GoogleTranslateWidget /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<VideoGuides />} />
+        <Route path="/itineraries" element={<Itineraries />} />
+        <Route path="/insights" element={<CityInsights />} />
+      </Routes>
+    </Router>
   );
 }
