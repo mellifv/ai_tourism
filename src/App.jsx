b3a618a6c3c0e268d302b88dcde6f9d102441e2e
mellifv <124873001+mellifv@users.noreply.github.com>
2025-12-02 19:54:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import VideoGuides from "./pages/VideoGuides";
 import Itineraries from "./pages/Itineraries";
@@ -7,17 +7,31 @@ import CityInsights from "./pages/CityInsights";
 import Nav from "./components/Nav";
 import { LanguageProvider } from "./context/LanguageContext";
 
+// Create a wrapper component to conditionally show Nav
+function AppContent() {
+  const location = useLocation();
+  
+  // Show Nav on ALL pages EXCEPT Itineraries
+  const showNav = location.pathname !== "/itineraries";
+  
+  return (
+    <>
+      {showNav && <Nav />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<VideoGuides />} />
+        <Route path="/itineraries" element={<Itineraries />} />
+        <Route path="/insights" element={<CityInsights />} />
+      </Routes>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <LanguageProvider>
       <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/videos" element={<VideoGuides />} />
-          <Route path="/itineraries" element={<Itineraries />} />
-          <Route path="/insights" element={<CityInsights />} />
-        </Routes>
+        <AppContent />
       </Router>
     </LanguageProvider>
   );
