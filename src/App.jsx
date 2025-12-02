d62edf10a363109fed02753ecd0b16592797dbac
mellifv <124873001+mellifv@users.noreply.github.com>
2025-12-02 16:23:34 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LanguageProvider } from "./context/LanguageContext"; // Add this
 import Home from "./pages/Home";
 import VideoGuides from "./pages/VideoGuides";
 import Nav from "./components/Nav";
 import Itineraries from "./pages/Itineraries";
 import CityInsights from "./pages/CityInsights";
-import GoogleTranslateWidget from "./pages/translate"; // 👈 new
 
 export default function App() {
   return (
-    <Router>
-      <Nav />
-      {/* Global language switcher */}
-      <GoogleTranslateWidget />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos" element={<VideoGuides />} />
-        <Route path="/itineraries" element={<Itineraries />} />
-        <Route path="/insights" element={<CityInsights />} />
-      </Routes>
-    </Router>
+    <LanguageProvider> {/* Wrap everything with LanguageProvider */}
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos" element={<VideoGuides />} />
+          <Route path="/itineraries" element={<Itineraries />} />
+          <Route path="/insights" element={<CityInsights />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
