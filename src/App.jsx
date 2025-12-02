b8ecd32675b825651b282e220c6b918482a9868d
mellifv <124873001+mellifv@users.noreply.github.com>
2025-12-02 17:00:17 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import VideoGuides from "./pages/VideoGuides";
 import Nav from "./components/Nav";
 import Itineraries from "./pages/Itineraries";
 import CityInsights from "./pages/CityInsights";
-// Remove this line: import GoogleTranslateWidget from "./pages/translate";
+import { LanguageProvider } from './context/LanguageContext';
 
 export default function App() {
   return (
-    <Router>
-      <Nav />
-      {/* Remove this: <GoogleTranslateWidget /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos" element={<VideoGuides />} />
-        <Route path="/itineraries" element={<Itineraries />} />
-        <Route path="/insights" element={<CityInsights />} />
-      </Routes>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos" element={<VideoGuides />} />
+          <Route path="/itineraries" element={<Itineraries />} />
+          <Route path="/insights" element={<CityInsights />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
+
