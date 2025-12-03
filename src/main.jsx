64f4430d84cb1c391ac5090ca90f737167882aac
mellifv <124873001+mellifv@users.noreply.github.com>
2025-12-03 14:09:14 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import { LanguageProvider } from './context/LanguageContext';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* ✅ WRAP App with LanguageProvider */}
+    <LanguageProvider>
       <App />
+    </LanguageProvider>
   </React.StrictMode>
 );
-
